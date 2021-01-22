20842d8b28cd8cce08e9754cb3e00498afe714c5
S.T <ar1515@qq.com>
2021-01-22 17:32:07 +0800

    add tutorial links
    tutorials about git and gitkraken

--- hellohello.cs
+++ hellohello.cs
@@ -4,5 +4,10 @@ public class Class1
 {
 	public Class1()
 	{
+		/*
+		 * 基础使用教程视频：gitkraken https://www.bilibili.com/video/BV1KD4y1S7FL?from=search&seid=16057128167242051865
+		 * Git命令大全：简书：https://www.jianshu.com/p/93318220cdce
+		 * 廖雪峰的git教程：https://www.liaoxuefeng.com/wiki/896043488029600
+		 */
 	}
 }
